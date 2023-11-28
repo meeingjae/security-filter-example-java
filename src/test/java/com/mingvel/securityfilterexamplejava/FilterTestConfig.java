50ef2ad1f15437083d961d6f256e50b17bc3582f
Minjae Kim <minjae.kim@mytrum.com>
2023-11-28 21:55:03 +0900

    filter mocking

---
+++ src/test/java/com/mingvel/securityfilterexamplejava/FilterTestConfig.java
@@ -0,0 +1,14 @@
+package com.mingvel.securityfilterexamplejava;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration
+public class FilterTestConfig {
+
+    @Bean
+    public ApiKeyFilter apiKeyFilter() {
+
+        return new ApiKeyFilter();
+    }
+}
